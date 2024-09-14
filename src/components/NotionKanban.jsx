bd7615f9b7c2075cc3321e83c24888426cf66e09
amine omari <omari1amineco@gmail.com>
2024-09-14 14:46:29 +0300

    feat: add new NotionKaban component

---
+++ src/components/NotionKanban.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const NotionKanban = () => {
+  return (
+    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+      <Board />
+    </div>
+  );
+};
+
+export default NotionKanban;
