dbca4be741c56841b69e9aa87198bc313244afe4
amine omari <omari1amineco@gmail.com>
2024-09-14 17:18:44 +0300

    feat: passing handleDragOver to onDragOver prop in BurnBarrel component

--- src/components/NotionKanban.jsx
+++ src/components/NotionKanban.jsx
@@ -120,6 +120,7 @@ const BurnBarrel = ({ setCards }) => {
 
   return (
     <div
+      onDragOver={handleDragOver}
       className={`mt-10 grid h-56 w-56 shrink-0 place-content-center rounded border text-3xl ${
         active
           ? "border-red-800 bg-red-800/20 text-red-500"
