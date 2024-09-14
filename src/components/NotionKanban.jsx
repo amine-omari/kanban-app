1c91d3103e0a018eda29e0059dc644c1be95ed4f
amine omari <omari1amineco@gmail.com>
2024-09-14 14:47:26 +0300

    feat: add new Column component

--- src/components/NotionKanban.jsx
+++ src/components/NotionKanban.jsx
@@ -18,7 +18,53 @@ const Board = () => {
 
   return (
     <div className="flex h-full w-full gap-3 overflow-scroll p-12">
-      
+      <Column
+        title="Backlog"
+        column="backlog"
+        headingColor="text-neutral-500"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="TODo"
+        column="todo"
+        headingColor="text-yellow-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="In progress"
+        column="doing"
+        headingColor="text-blue-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="Complete"
+        column="done"
+        headingColor="text-emerald-200"
+        cards={cards}
+        setCards={setCards}
+      />
+    </div>
+  );
+};
+
+const Column = ({ title, headingColor, column, cards, setCards }) => {
+  const [active, setActive] = useState(false);
+  const filterCards = cards.filter((c) => c.column === column);
+
+  return (
+    <div className="w-56 shrink-0">
+      <div className="mb-3 flex items-center justify-between">
+        <h3 className={` font-medium ${headingColor}`}>{title}</h3>
+        <span className="rounded text-sm text-neutral-400">{cards.length}</span>
+      </div>
+      <div
+        className={`h-full w-full transition-colors ${
+          active ? "bg-neutral-800/50" : "bg-neutral-800"
+        }`}
+      ></div>
     </div>
   );
 };
