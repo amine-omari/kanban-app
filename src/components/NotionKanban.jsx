18020897e3104e6ed380afbf4851cdd32944129c
amine omari <omari1amineco@gmail.com>
2024-09-15 18:51:45 +0300

    feat: add getNearesetIndicator function

--- src/components/NotionKanban.jsx
+++ src/components/NotionKanban.jsx
@@ -69,16 +69,30 @@ const Column = ({ title, headingColor, column, cards, setCards }) => {
 
   const highlightIndicator = (e) => {
     const indicators = getIndicators();
+    const el = getNearrestIndicator(e, indicators);
   };
 
   const getNearrestIndicator = (e, indicators) => {
+    const DISTANCE_OFFSET = 50;
+
     const el = indicators.reduce(
-      (closest, child) => {},
-      (
-        offset:Number.NEGATIVE_INFINITY,
-        element: indicators[indicators.length-1],
-      ),
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = e.clientY - (box.top - DISTANCE_OFFSET);
+
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      {
+        offset: Number.NEGATIVE_INFINITY,
+        element: indicators[indicators.length - 1],
+      }
     );
+
+    return el;
   };
 
   const getIndicators = () => {
